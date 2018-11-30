5c5b8a876c3bf308068c01995642bbdb46f1d77d
andriejka <step.andriej@gmail.com>
2018-11-30 03:08:08 +0100

    Update ImagesAsync.js

--- src/services/api/ImagesAsync.js
+++ src/services/api/ImagesAsync.js
@@ -1,6 +1,8 @@
 const API_KEY = "10786612-1f7c4a86cd3af4ed0123794b0";
 let URL = "https://pixabay.com/api/?key=" + API_KEY;
 
+//api key should be hidden
+
 const getImages = async (category, page) => {
   const chooseCategory = URL + "&orientation=horizontal&page=" + page + "&image_type=photo&category=" + category;
   const response = await fetch(chooseCategory);
